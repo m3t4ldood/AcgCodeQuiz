71a56c763695f280df8113bee2f8238dc2e3445e
Anthony Gannuscio <m3t4ldood@gmail.com>
2020-12-16 09:10:19 -0800

    Update script.js
    watched tutorial on how to set up this js file. VERY HELPFUL!!! :)

--- resources/Js/script.js
+++ resources/Js/script.js
@@ -1,3 +1,60 @@
+// from video Building a Multiple Choice Quiz | Javascript | Tutorial 24
+// Questions Array
+var questions = [
+    {
+        prompt: "----?\n(a) --\n\(b) ---\n\(c) ----",
+        answer: "a"
+      },
+      {
+        prompt: "----?\n(a) --\n\(b) ---\n\(c) ----",
+        answer: "c"
+      },
+      {
+        prompt: "----?\n(a) --\n\(b) ---\n\(c) ----",
+        answer: "a"
+      },
+      {
+        prompt: "----?\n(a) --\n\(b) ---\n\(c) ----",
+        answer: "c"
+      },
+      {
+        prompt: "----?\n(a) --\n\(b) ---\n\(c) ----",
+        answer: "a"
+      },
+      {
+        prompt: "----?\n(a) --\n\(b) ---\n\(c) ----",
+        answer: "c"
+      },
+      {
+        prompt: "----?\n(a) --\n\(b) ---\n\(c) ----",
+        answer: "a"
+      },
+      {
+        prompt: "----?\n(a) --\n\(b) ---\n\(c) ----",
+        answer: "c"
+      },
+      {
+        prompt: "----?\n(a) --\n\(b) ---\n\(c) ----",
+        answer: "a"
+      },
+      {
+        prompt: "----?\n(a) --\n\(b) ---\n\(c) ----",
+        answer: "c"
+      },
+]
+// basic for loop structure
+var score = 0;
+for(var i=0 i < questions.length; i++){
+  var response = window.prompt(questions[i].prompt)
+  if(response == question[i].answer)
+    score++;
+    alert("Correct!");
+  } else {
+    alert("WRONG!");
+  }
+}
+
+
 var startButton=document.getElementById("start-button")
 function startQuiz(){
 console.log("hello")
